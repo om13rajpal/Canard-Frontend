75b85443829fdbaaee0e89769233b2708a070bfd
Ishan Vohra <ivohra_be23@thapar.edu>
2025-01-28 20:21:30 +0530

    changes icons

--- src/components/Emote.jsx
+++ src/components/Emote.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-const Emote = ({ width, setAnimationUrl }) => {
+const Emote = ({ width, setAnimationUrl,imgSrc }) => {
+
+  
   return (
     <div
       className="flex justify-center items-center border-[2px] border-[#bbbbbb] rounded-[50%] p-2 ml-5 opacity-75"
       onClick={setAnimationUrl}
     >
-      <img src="/hamburger.png" width={width}  />
+      <img src={imgSrc} width={width}  />
     </div>
   );
 };
